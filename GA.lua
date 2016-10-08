b121bc964bfa648d83ac889f3fd76beb19325ef6
mute mute <mute mute>
2016-10-08 21:14:12 +0900

    finished

--- GA.lua
+++ GA.lua
@@ -4,13 +4,13 @@ require("player_api/machine")
 -- 0 1 2の３つの状態を遷移する
 function generate_gene(size,number)
   local gene = {}
-  for j = 0,number do
+  for j = 1,number do
     gene[j] = {}
-    for i = 0,size do
+    for i = 1,size do
       -- 0 right
       -- 1 up
       -- 2 down
-      gene[j][i] = math.random(0,1)
+      table.insert(gene[j],math.random(0,1))
     end
   end
   return gene
@@ -22,32 +22,23 @@ function calc(size,number,gene)
   local judge = 0
   local gene_result = {}
 
-  --gene_resultの初期化
-  for i = 0,number do
-    gene_result[i] = {1,1}
-  end
-
   --calc
-  for j = 0,number do
+  for j = 1,number do
     poweron()
-    for i = 0,size do
+    for i = 1,size do
       status = gene[j][i]
       if(status == 0) then
         move_right(framerate)
 
       elseif(status == 1) then
         move_up(framerate)
-
-      elseif(status == 2) then
-        move_wait(framerate)
-
       end
 
       if(is_dead() == 0) then
-        gene_result[j] = {1,emu.framecount()}
+        table.insert(gene_result,emu.framecount())
         break
       elseif(is_clear() == 0) then
-        gene_result[j] = {0,emu.framecount()}
+        table.insert(gene_result,emu.framecount())
         break
       end
 
@@ -57,27 +48,48 @@ function calc(size,number,gene)
 end
 
 function gene_sort(size,number,gene,gene_result)
-  before = gene_result[0][1]
-  before_ = gene_result[0][2]
-  end_flag = 0
-
   while true do
-    for i = 1,number do
-      if(before < gene_result[i][1] or before_ > gene_result[i][2]) then
-        end_flag = 1
-        tmp =  gene[i]
-        gene[i] = gene[i-1]
-        gene[i-1] = gene[i]
+    for i = 2,table.maxn(gene_result) do
+      if(gene_result[i-1] < gene_result[i]) then
+        flag = false
+        local temp = {unpack(gene[i-1])}
+        gene[i-1] = {unpack(gene[i])}
+        gene[i] = {unpack(temp)}
+
+        local temp = gene_result[i-1]
+        gene_result[i-1] = gene_result[i]
+        gene_result[i] = temp
       end
-      before = gene_result[i][1]
-      before_ = gene_result[i][2]
     end
-    if(end_flag == 0) then
+    if(flag == true) then
       break
     end
-    end_flag = 0
+    flag = true
   end
+
+  emu.print(gene_result)
+
   return gene
+end
+
+
+function crossing(parent,parent_) --1点交叉法
+  local child = {}
+  local child_ = {}
+
+  point = math.random(1,table.maxn(parent))
+
+  for i = 1,point do
+    table.insert(child,parent[i])
+    table.insert(child_,parent_[i])
+  end
+
+  for i = point,table.maxn(parent) do
+    table.insert(child,parent_[i])
+    table.insert(child_,parent[i])
+  end
+
+  return (child),(child_)
 end
 
 --ランキング選択
@@ -86,9 +98,46 @@ end
 -- 3位 15%
 -- 4位 10%
 -- 5位 5%
-function selection(size,number,gene,gene_result) --ランキング選択
-  return gene
-end
+function generate_nextgen(size,number,gene,gene_result) --ランキング選択
+  gene = gene_sort(size,number,gene,gene_result)
+  selection_list = {}
 
-function crossing(gene,gene_) --多点交叉法
+  --ランキング方式の割合設定
+  first = 50
+  second = 20
+  third = 15
+  fourth = 10
+  fifth = 5
+
+  for i = 1,first do
+    table.insert(selection_list,gene[i])
+  end
+
+  for i = 1,second do
+    table.insert(selection_list,gene[i+1])
+  end
+
+  for i = 1,third do
+    table.insert(selection_list,gene[i+2])
+  end
+
+  for i = 1,fourth do
+    table.insert(selection_list,gene[i+3])
+  end
+
+  for i = 1,fifth do
+    table.insert(selection_list,gene[i+4])
+  end
+
+  local next_gene = {}
+  for i = 0,number/2 do
+    parent = selection_list[math.random(1,table.maxn(selection_list))]
+    parent_ = selection_list[math.random(1,table.maxn(selection_list))]
+
+    child,child_ = crossing(parent,parent_)
+    table.insert(next_gene,child)
+    table.insert(next_gene,child_)
+  end
+
+  return next_gene
 end
--- main.lua
+++ main.lua
@@ -2,17 +2,22 @@ require("GA")
 
 --集団サイズは100 * 10
 -- size * number
-size = 500
-number = 4 -- number >= 4
+size = 100
+number = 50 -- number >= 5
 
 final_generation = 100
 
 gene = generate_gene(size,number)
 now_generation = 0
 
+
 while (now_generation <= final_generation) do
+  emu.print(now_generation)
+
   gene_result = calc(size,number,gene)
-  gene = gene_sort(size,number,gene,gene_result)
+  gene = generate_nextgen(size,number,gene,gene_result)
 
   now_generation = 1 + now_generation
 end
+
+emu.print("finished")
